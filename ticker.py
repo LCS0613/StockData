f868bbd676bb6f91af918ee8c1d3d98d0951235b
Lim CS <108396572+LCS0613@users.noreply.github.com>
2023-06-28 00:41:18 +0900

    get_stock_information 함수 추가

--- ticker.py
+++ ticker.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import yfinance as yf
 import investpy
+import requests
 
 def kospi_ticker():
-
     # 한국 주식 상장 목록 불러오기
     kospi_data = investpy.get_stocks(country='South Korea')
     kospi_data = kospi_data.loc[:, ['symbol', 'name']]
@@ -22,21 +22,25 @@ def nasdaq_ticker():
 def get_stock_information(tickers):
     stock_information = []
     for ticker in tickers:
-        stock_info = yf.Ticker(ticker).info
-        stock_information.append([stock_info['symbol'], stock_info.get('shortName', None),
-                                  stock_info.get('trailingPE', None),
-                                  stock_info.get('enterpriseToEbitda', None), stock_info.get('priceToBook', None),
-                                  stock_info.get('priceToSales', None), stock_info.get('enterpriseToRevenue', None),
-                                  stock_info.get('forwardPE', None), stock_info.get('trailingEps', None),
-                                  stock_info.get('roe', None)])
+        try:
+            stock_info = yf.Ticker(ticker).info
+            stock_information.append([stock_info['symbol'], stock_info.get('shortName', 'unavailable'),
+                                      stock_info.get('trailingPE', None),
+                                      stock_info.get('enterpriseToEbitda', None), stock_info.get('priceToBook', None),
+                                      stock_info.get('priceToSales', None), stock_info.get('enterpriseToRevenue', None),
+                                      stock_info.get('forwardPE', None), stock_info.get('trailingEps', None),
+                                      stock_info.get('roe', None)])
+        except requests.exceptions.HTTPError:
+            print(f"{ticker}: 데이터를 가져오는 동안 문제가 발생했습니다. 이 종목은 건너뜁니다.")
 
     columns = ['symbol', 'name', 'p/e', 'ev/ebitda', 'p/b', 'p/s', 'ev/revenue', 'forward p/e', 'eps', 'roe']
     stock_information_df = pd.DataFrame(stock_information, columns=columns)
 
-    print(stock_information_df)
+    return stock_information_df
 
 
 if __name__ == '__main__':
     print(kospi_ticker())
     print(nasdaq_ticker())
-    print(get_stock_information(['AAPL', 'MSFT']))
+    nasdaq_ticker_list = nasdaq_ticker()['Ticker'].tolist()
+    print(get_stock_information(nasdaq_ticker_list[:10]))
